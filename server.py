247361e835c57cf86c4a804c61d095418c88e343
piupaulgit <contact.piu619@gmail.com>
2020-06-06 14:03:53 +0530

    login added

--- server.py
+++ server.py
@@ -55,91 +55,21 @@ def register():
 
     return jsonify(result)
 
+# login api
+@app.route('/user/login', methods=['POST'])
+def login():
+    cur = mysql.connection.cursor()
+    email = request.get_json()['email']
+    password = request.get_json()['password']
+    result = ''
 
+    cur.execute("SELECT * FROM users where email = '" + str(email) + "'")
+    rv = cur.fetchone()
 
-
-
-    # return jsonify({"name":name, "email":email, "created": created, "password":password})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# mysql = MySQL(app)
-
-
-# @app.route('/')
-# def hello_world():
-#     cur = mysql.connection.cursor()
-#     cur.execute("SELECT  * FROM users")
-#     data = cur.fetchall()
-#     cur.close()
-#     return render_template('./index.html', users=data)
-
-
-# @app.route('/insert', methods=['POST'])
-# def insert():
-
-#     if request.method == "POST":
-#         flash("Data Inserted Successfully")
-#         name = request.form['name']
-#         email = request.form['email']
-#         password = request.form['password']
-#         cur = mysql.connection.cursor()
-#         cur.execute(
-#             "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)", (name, email, password))
-#         mysql.connection.commit()
-
-#         return jsonify({'response': 'inserted'})
-#         # return redirect(url_for('/'))
-
-
-# @app.route('/delete/<string:id_data>', methods=['GET'])
-# def delete(id_data):
-#     cur = mysql.connection.cursor()
-#     cur.execute("DELETE FROM users WHERE id=%s", (id_data,))
-#     mysql.connection.commit()
-#     return jsonify('Record Has Been Deleted Successfully')
-
-
-# @app.route('/about')
-# def about():
-#     return 'About Piu!'
+    if bcrypt.check_password_hash(rv['password'],password):
+        access_token = bcrypt.create_access_token(identity = {"name" : rv['name'], "email" : rv['email'], "id" : rv['id']})
+        result = access_token
+    else:
+        result = jsonify({"error" : "user not found"})
+    
+    return result
